c5f4e9556ca5aaf4913a70aca716aeec9b08a139
Edimar de Sá <edimar.sa@efscode.com.br>
2025-05-06 16:26:01 -0300

    Temp commit

--- docker/down.sh
+++ docker/down.sh
@@ -3,4 +3,4 @@
 echo "Parando os containers"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-docker compose -f $SCRIPT_DIR/compose.yaml down --remove-orphans
+docker compose -f $SCRIPT_DIR/docker-compose.yaml down --remove-orphans -v
