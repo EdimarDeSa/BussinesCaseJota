22a5c88543ae75f2c8291d9b87a531f0d763e633
Edimar de Sá <edimar.sa@efscode.com.br>
2025-05-07 21:03:42 -0300

    Configurando rabbitmq

--- docker/dev_up.sh
+++ docker/dev_up.sh
@@ -4,4 +4,4 @@ echo "Iniciando o containers para desenvolvimento"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-docker compose -f "$SCRIPT_DIR/docker-compose.yaml" up -d postgres redis
+docker compose -f "$SCRIPT_DIR/docker-compose.yaml" up -d postgres redis rabbitmq
