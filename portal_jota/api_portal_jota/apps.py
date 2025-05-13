256677e3d6b9d16a585cff7673468a8dfba5b16f
Edimar de Sá <edimar.sa@efscode.com.br>
2025-05-12 22:48:42 -0300

    Finalização da configuração otimizada para worker

--- portal_jota/api_portal_jota/apps.py
+++ portal_jota/api_portal_jota/apps.py
@@ -1,9 +1,14 @@
+import os
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class ApiPortalJotaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api_portal_jota"
 
-    def ready(self):
+    def ready(self) -> None:
+        if os.getenv("ENV_TYPE", "full").lower() not in ["full", "web"]:
+            return
         from . import signals
