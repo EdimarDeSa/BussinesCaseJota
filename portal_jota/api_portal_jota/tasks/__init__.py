6b242af22edc88ae0f4e15382cfc6a23c377e6b0
Edimar de Sá <edimar.sa@efscode.com.br>
2025-05-13 12:20:49 -0300

    Ajustando método para fazer o import condicional

--- portal_jota/api_portal_jota/tasks/__init__.py
+++ portal_jota/api_portal_jota/tasks/__init__.py
@@ -1,2 +1,14 @@
-from .process_image import process_image
-from .send_email import send_email
+from django.conf import settings
+
+match settings.ENV_TYPE:
+    case "beat":
+        from .publicar_noticia import publicar_noticia
+
+    case "worker":
+        from .process_image import process_image
+        from .send_email import send_email
+
+    case _:
+        from .process_image import process_image
+        from .publicar_noticia import publicar_noticia
+        from .send_email import send_email
