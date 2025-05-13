758c27e1fd929068e519ea43eecf50a5443e68db
Edimar de Sá <edimar.sa@efscode.com.br>
2025-05-13 13:29:29 -0300

    Ajustes menores

--- portal_jota/api_portal_jota/apps.py
+++ portal_jota/api_portal_jota/apps.py
@@ -1,5 +1,3 @@
-import os
-
 from django.apps import AppConfig
 from django.conf import settings
 
@@ -9,6 +7,6 @@ class ApiPortalJotaConfig(AppConfig):
     name = "api_portal_jota"
 
     def ready(self) -> None:
-        if os.getenv("ENV_TYPE", "full").lower() not in ["full", "web"]:
+        if settings.ENV_TYPE not in ["full", "web"]:
             return
         from . import signals
